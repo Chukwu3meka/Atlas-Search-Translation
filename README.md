f5825eed6d74a48dc813c94f783362381903d952
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-04-28 20:38:28 +0100

    added local db to hold text added to cloud

--- README.md
+++ README.md
@@ -24,3 +24,5 @@ Translation is an act, process, or instance of translating: such as. a : a rende
 
 <!-- Marcus -->
 <!-- https://github.com/MarcusSorealheis -->
+
+<!-- https://cloud.mongodb.com/v2/6266f035f8818000903c457b#metrics/replicaSet/6266f149a9ab1b5880de47b9/explorer/translations/greetings/find -->
--- pages/api/admin/deleteTranslations.js
+++ pages/api/admin/deleteTranslations.js
@@ -1,16 +1,13 @@
 export default async (req, res) => {
   try {
-    const translations = require("@source/translations").default;
-
     const { Greetings } = await require("@db").default();
 
     // // you don't need to tell an attacker, he/she sent a wrong key. Let them go through the pain
     if (req.body.adminKey !== process.env.ADMIN_KEY) throw "service unavailable/wrong URL queried";
 
-    //   // insertMany is best for adding multiple documents to MongoDB, we could have used `.insert` or `.create`, but in a situation where we hav 70,000 files, that won't be wise
-    await Greetings.insertMany(translations);
-
-    // console.log(translations);
+    // Removes all documents that match the filter from a collection.
+    // but not the collection itself
+    await Greetings.deleteMany({});
 
     return res.status(200).json("success");
   } catch (error) {
