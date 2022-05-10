dc008b6758a3976ebb929e4633c890eb00237e38
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-05-10 14:32:51 +0100

    added text/icons to feedback

--- component/feedback/FeedbackContainer.jsx
+++ component/feedback/FeedbackContainer.jsx
@@ -6,7 +6,10 @@ import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { useState } from "react";
-import { Typography } from "@mui/material";
+import { Stack, Box, Paper, Typography } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOutlinedIcon from "@mui/icons-material/ThumbDownOutlined";
 
 const FeedbackContainer = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -41,12 +44,41 @@ const FeedbackContainer = () => {
         MenuListProps={{
           "aria-labelledby": "basic-button",
         }}>
-        <Typography variant="body1">Are you satisfied with this translation?</Typography>
-        <Button>Suggest an edit</Button>
-        <Typography variant="body2">Your feedback will be used to help improve the product</Typography>
-        {/* <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <Paper elevation={0} sx={{ borderRadius: 20 }}>
+          <Box display="flex" flexDirection="column" p={1.5} maxWidth={230}>
+            <Typography
+              variant="body1"
+              sx={{
+                fontWeight: "bold",
+                textAlignLast: "center",
+              }}>
+              Are you satisfied with this translation?
+            </Typography>
+
+            <Stack direction="row" spacing={3} key={"label"} justifyContent="center" alignItems="center" mt={1.6} mb={0.4}>
+              <IconButton aria-label={"label"} sx={{ border: "1px solid #dad7d7", padding: 1.5 }}>
+                <ThumbUpOutlinedIcon />
+              </IconButton>
+              <IconButton aria-label={"label"} sx={{ border: "1px solid #dad7d7", padding: 1.5 }}>
+                <ThumbDownOutlinedIcon />
+              </IconButton>
+            </Stack>
+
+            <Button
+              sx={{
+                textTransform: "none",
+                mb: 1.1,
+              }}>
+              <Typography variant="body2" color="primary">
+                <b>Suggest an edit</b>
+              </Typography>
+            </Button>
+            <Typography variant="caption">Your feedback will be used to help improve the product</Typography>
+            {/* <MenuItem onClick={handleClose}>Profile</MenuItem>
         <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem> */}
+      <MenuItem onClick={handleClose}>Logout</MenuItem> */}
+          </Box>
+        </Paper>
       </Menu>
     </>
   );
