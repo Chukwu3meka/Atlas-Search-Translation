d6b3b251d5ec8370f1d2b2ded13e59bcc84d27cc
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-06-02 17:43:55 +0100

    added env to server and fixed client deployment error

--- pages/api/admin/addTranslations.js
+++ pages/api/admin/addTranslations.js
@@ -3,36 +3,40 @@ const fs = require("fs");
 export default async (req, res) => {
   try {
     const translations = require("@source/ongoingTranslations").default;
-    const translationsInDatabase = require("@source/databaseTranslations").default;
 
-    const { Greetings } = await require("@db").default();
+    // console.log(translations.length);
+    // const translationsInDatabase = require("@source/databaseTranslations").default;
 
-    // you don't need to tell an attacker, he/she sent a wrong key. Let them go through the pain
-    if (process.env.NODE_ENV !== "development") throw "service unavailable/wrong URL queried";
+    // const { Greetings } = await require("@db").default();
 
-    // invalid translations
-    const invalidTranslations = [];
+    // // you don't need to tell an attacker, he/she sent a wrong key. Let them go through the pain
+    // if (process.env.NODE_ENV !== "development") throw "service unavailable/wrong URL queried";
 
-    // remove translations already in db
-    const nonDuplicateTranslation = await translations.filter((x) => {
-      if (
-        x.english &&
-        x.french &&
-        x.spanish &&
-        !translationsInDatabase.find((y) => y.english.toLowerCase() === x.english.toLowerCase())
-      ) {
-        return x;
-      } else {
-        invalidTranslations.push(x);
-      }
-    });
+    // // invalid translations
+    // const invalidTranslations = [];
 
-    //   // insertMany is best for adding multiple documents to MongoDB, we could have used `.insert` or `.create`, but in a situation where we hav 70,000 files, that won't be wise
+    // // remove translations already in db
 
-    await Greetings.insertMany(nonDuplicateTranslation);
+    const nonDuplicateTranslation = [];
+    // const nonDuplicateTranslation = await translations.filter((x) => {
+    //   if (
+    //     x.english &&
+    //     x.french &&
+    //     x.spanish &&
+    //     !translationsInDatabase.find((y) => y.english.toLowerCase() === x.english.toLowerCase())
+    //   ) {
+    //     return x;
+    //   } else {
+    //     invalidTranslations.push(x);
+    //   }
+    // });
 
-    // write invalidTranslations to logs.invalidTranslations.js in root file
-    fs.writeFile("./logs.invalidTranslations.js", `export default ${JSON.stringify(invalidTranslations)}`, (err) => console.log(err));
+    // //   // insertMany is best for adding multiple documents to MongoDB, we could have used `.insert` or `.create`, but in a situation where we hav 70,000 files, that won't be wise
+
+    // await Greetings.insertMany(nonDuplicateTranslation);
+
+    // // write invalidTranslations to logs.invalidTranslations.js in root file
+    // fs.writeFile("./logs.invalidTranslations.js", `export default ${JSON.stringify(invalidTranslations)}`, (err) => console.log(err));
 
     return res.status(200).json(`Sucessfully added ${nonDuplicateTranslation.length} documents`);
   } catch (error) {
