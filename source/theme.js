6f3b53683d7e5971425aec3970108917a12273c6
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-06-23 22:17:25 +0100

    fixing input not working on iPhone

--- source/theme.js
+++ source/theme.js
@@ -15,16 +15,25 @@ const muiTheme = createTheme({
     // main: "#e2ad26",
     // },
   },
+
   components: {
     MuiTextField: {
+      // defaultProps: {
+      //   InputProps: {
+      //     "-webkit-user-select": "text" /* Chrome, Opera, Safari */,
+      //     "-moz-user-select": "text" /* Firefox 2+ */,
+      //     "-ms-user-select": "text" /* IE 10+ */,
+      //     "user-select": "text" /* Standard syntax */,
+      //   },
+      //   inputProps: {
+      //     "-webkit-user-select": "text" /* Chrome, Opera, Safari */,
+      //     "-moz-user-select": "text" /* Firefox 2+ */,
+      //     "-ms-user-select": "text" /* IE 10+ */,
+      //     "user-select": "text" /* Standard syntax */,
+      //   },
+      // },
       styleOverrides: {
         root: {
-          "-webkit-user-select": "text !important" /* Chrome, Opera, Safari */,
-          "-moz-user-select": "text !important" /* Firefox 2+ */,
-          "-ms-user-select": "text !important" /* IE 10+ */,
-          "user-select": "text !important" /* Standard syntax */,
-          // border: "3px solid red !important",
-
           "*": {
             "-webkit-user-select": "text !important" /* Chrome, Opera, Safari */,
             "-moz-user-select": "text !important" /* Firefox 2+ */,
