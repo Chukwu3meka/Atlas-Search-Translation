f2e67a7d41c5a2930f8a96acb79bca49d865e40b
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-08-01 10:22:24 +0100

    fixing api usage with next/pages/api

--- component/textTranslate/index.js
+++ component/textTranslate/index.js
@@ -1,21 +1,21 @@
+import Vote from "./Vote";
 import Container from "./Container";
 import SourceText from "./SourceText";
-import Translation from "./Translation";
 import Suggestion from "./Suggestion";
+import Translation from "./Translation";
+import VoteContainer from "./VoteContainer";
 import TranslationContainer from "./TranslationContainer";
 import SourceTextContainer from "./SourceTextContainer";
-import VoteContainer from "./VoteContainer";
 import SuggestionContainer from "./SuggestionContainer";
-import Vote from "./Vote";
 
 export {
   Vote,
-  SourceText,
-  SuggestionContainer,
   Suggestion,
+  SourceText,
+  Translation,
   VoteContainer,
+  SuggestionContainer,
   SourceTextContainer,
   TranslationContainer,
-  Translation,
   Container as default,
 };
