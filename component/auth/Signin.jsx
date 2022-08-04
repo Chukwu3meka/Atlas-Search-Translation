48c8fae0572acfd7d2f691fbd43a9acb3859169b
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-08-04 15:25:25 +0100

    fixed mui/lab error

--- component/auth/Signin.jsx
+++ component/auth/Signin.jsx
@@ -1,7 +1,6 @@
-import { Box, Stack, TextField, Typography, InputLabel, IconButton, FormControl, OutlinedInput, InputAdornment } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
-// import LoadingButton from "@mui/lab/LoadingButton";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Box, Stack, TextField, Typography, InputLabel, IconButton, FormControl, OutlinedInput, InputAdornment } from "@mui/material";
 
 const Signin = ({ values, loading, handleChange, signinHandler, handleClickShowPassword, handleMouseDownPassword }) => (
   <Box px={1.5} py={5} maxWidth={500} m="auto">
