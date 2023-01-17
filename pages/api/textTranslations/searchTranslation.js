f7122440604c7bb4ec0d6f0383d1ebb27dc154b2
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-17 22:12:24 +0100

    updated searchTranslation query

--- pages/api/textTranslations/searchTranslation.js
+++ pages/api/textTranslations/searchTranslation.js
@@ -4,15 +4,15 @@ const handler = async (req, res) => {
   try {
     const { sourceText, sourceLanguage, translationLanguage } = objectValuesToLowerCase(req.body);
 
-    if ([sourceText, sourceLanguage, translationLanguage].some((x) => x === null || x === undefined))
-      throw { label: "Search phrase cannot be empty" };
+    if ([sourceText, sourceLanguage, translationLanguage].some((x) => x === null || x === undefined)) throw { label: "Search phrase cannot be empty" };
 
-    const searchIndex = sourceLanguage === "english" ? "englishSearch" : sourceLanguage === "french" ? "frenchSearch" : "spanishIndex";
+    // const searchIndex = sourceLanguage === "english" ? "englishSearch" : sourceLanguage === "french" ? "frenchSearch" : "spanishIndex";
 
     const { Translations } = await require("@db").default();
 
     const result = await Translations.aggregate([
-      { $search: { index: searchIndex, text: { query: sourceText, path: sourceLanguage } } },
+      // { $search: { index: searchIndex, text: { query: sourceText, path: sourceLanguage } } },
+      { $search: { text: { query: sourceText, path: sourceLanguage } } },
       { $limit: 1 },
       // { $project: { _id: 0 } },
       { $project: { score: { $meta: "searchScore" }, [sourceLanguage]: 1, [translationLanguage]: 1 } },
@@ -25,7 +25,7 @@ const handler = async (req, res) => {
       result: result[0] ? result[0][translationLanguage] : "no translation found",
     };
 
-    console.log(translation);
+    // console.log(translation);
 
     res.status(200).json(translation);
   } catch (err) {
